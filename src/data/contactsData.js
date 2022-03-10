1f2f70dd45235d3a4eec48f6d42ecd2d810e091b
SANGARIGURU <sangariguru03@gmail.com>
2022-03-10 23:57:57 +0530

    portfolio

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
 	email: "sangariguru03@gmail.com",
-	phone: "9597136971",
+	phone: "+91 9597136971",
 	address: "Tiruchirappalli,Tamil Nadu",
 	github: "https://github.com/sivasangari03-G",
 	linkedIn: "https://www.linkedin.com/in/sivasangari-g-9035b01bb/",
