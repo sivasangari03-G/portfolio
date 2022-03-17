5dcb89b847f3da110d460277052bc7698cf861cf
SANGARIGURU <sangariguru03@gmail.com>
2022-03-17 11:58:24 +0530

    updated

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,5 +1,7 @@
 export const socialsData = {
 	github: "https://github.com/sivasangari03-G",
 	linkedIn: "https://www.linkedin.com/in/sivasangari-g-9035b01bb/",
-	codepen: "https://codesandbox.io/u/sivasangari03-G",
+	reddit: "https://codesandbox.io/u/sivasangari03-G",
+	codepen: "https://codepen.io/sivasangari03-g",
+	stackOverflow: "https://stackoverflow.com/users/18481998/sivasangari",
 };
